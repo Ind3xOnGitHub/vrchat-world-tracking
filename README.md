4396a8f44ae8a283284122d723eae8aba95aa9ae
Ind3xOnGitHub <me@ind3x.site>
2018-08-01 18:12:31 +0200

    Move worlds to worlds.json

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Example: https://analytics.vrchat.ind3x.site (Build: [![CircleCI](https://circleci.com/gh/Ind3xOnGitHub/vrchat-world-tracking/tree/master.svg?style=svg)](https://circleci.com/gh/Ind3xOnGitHub/vrchat-world-tracking/tree/master))
 
 ```shell
-$ scp consolidated_occupants.txt cronjob.js index.html user@host:/path/to/your/webroot
+$ scp consolidated_occupants.txt cronjob.js index.html worlds.json user@host:/path/to/your/webroot
 ```
 
 ```shell
@@ -13,6 +13,6 @@ $ chmod 0700 cronjob.js
 $ crontab -e
 
 API_KEY="your api key"
-FILE_PATH="/path/to/your/webroot/"
+ROOT_PATH="/path/to/your/webroot"
 */5 * * * * /path/to/your/node /path/to/your/webroot/crontob.js
 ```
--- cronjob.js
+++ cronjob.js
@@ -2,17 +2,8 @@ const fs = require('fs')
 const https = require('https')
 
 const apiKey = process.env.API_KEY
-const worlds = {
-  'sky_club': {
-    id: 'wrld_4cda3f9c-2519-43a9-96a2-3c456d71a52a'
-  },
-  'indexs_home': {
-    id: 'wrld_a3a0050c-24a0-4b99-9ed7-a63073187050'
-  },
-  'club_galaxy': {
-    id: 'wrld_860d0a51-a539-4cc5-860e-bd5f82d3a6d3'
-  }
-}
+const rootPath = process.env.ROOT_PATH || './';
+const worlds = require(`${rootPath}worlds.json`)
 
 
 function request(worldKey, world, callback) {
@@ -51,7 +42,7 @@ function saveConsolidatedOccupoants() {
     line += ';' + worlds[worldKey].rawResponse.occupants
   }
 
-  const path = process.env.FILE_PATH ? `${process.env.FILE_PATH}consolidated_occupants.txt` : 'consolidated_occupants.txt'
+  const path = `${rootPath}consolidated_occupants.txt`
   fs.appendFile(path, line + "\n", (err) => {
     if (err) console.error(err)
   });
